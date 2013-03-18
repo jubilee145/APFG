75ff84e8287a77349cc59d31305e304829f16f1e
jubilee145 <jubileech@gmail.com>
2013-03-18 11:46:06 +1100

    Cleaned up Manager (slightly).
    Also solved super-jump bug, and added a basic scaling system to the render() method.
    
    fixes #7

--- src/svb/Manager.java
+++ src/svb/Manager.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Rectangle;
@@ -73,6 +71,10 @@ public class Manager
 	
 	public static void update(GameContainer container, int delta) throws SlickException
 	{
+		//Make sure the game doesn't keep going when the container is frozen. :P
+		if (delta > 20)
+			delta = 20;
+
 		for(Fighter f: fighters)
 		{
 			for (Fighter f2 : fighters)
@@ -165,12 +167,15 @@ public class Manager
 	
 	public static void render(GameContainer container, Graphics g) throws SlickException
 	{
+		/**
+		 * The graphics context should start at 1920x1080, and then scale down to
+		 * whatever system it's working on. (ContainerWidth / 1920) ?
+		 */
+		g.scale(container.getWidth() / 1920f, container.getHeight() / 1080f);
 		for(Camera c : cameras)
 		{
 			c.render(g);
 		}
-		if(debug)
-			g.drawString("Actors: " + actors.size(), 5, 35);
 	}
 	
 	private static boolean seperateX(Actor actor1, Actor actor2)
@@ -200,24 +205,14 @@ public class Manager
 				if(act1delta > act2delta)
 				{
 					overlap = actor1.location.x + act1rect.getWidth() - actor2.location.x;
-					if((overlap > maxOverlap))// || !(actor1.allowCollisions & RIGHT) || !(actor2.allowCollisions & LEFT))
+					if((overlap > maxOverlap))
 						overlap = 0;
-					/*else
-					{
-						actor1.touching |= RIGHT;
-						actor2.touching |= LEFT;
-					}*/
 				}
 				else if(act1delta < act2delta)
 				{
 					overlap = actor1.location.x - act2rect.getWidth() - actor2.location.x;
-					if((-overlap > maxOverlap))// || !(actor1.allowCollisions & LEFT) || !(actor2.allowCollisions & RIGHT))
+					if((-overlap > maxOverlap))
 						overlap = 0;
-					/*else
-					{
-						actor1.touching |= LEFT;
-						actor2.touching |= RIGHT;
-					}*/
 				}
 			}
 		}
@@ -251,98 +246,6 @@ public class Manager
 			{
 				actor2.location.x += overlap;
 				actor2.velocity.x = act1v - act2v*actor2.elasticity;
-			}
-			return true;
-		}
-		else
-			return false;
-	}
-	
-	private static boolean seperateY(Actor actor1, Actor actor2)
-	{
-		//can't separate two immovable objects
-		Boolean act1immovable = actor1.immovable;
-		Boolean act2immovable = actor2.immovable;
-		if(act1immovable && act2immovable)
-			return false;
-		
-		//If one of the objects is a tilemap, just pass it off.
-		/*if(actor1 is FlyTilemap)
-			return (actor1 as FlyTilemap).overlapsWithCallback(actor2,separatey);
-		if(actor2 is FlyTilemap)
-			return (actor2 as FlyTilemap).overlapsWithCallback(actor1,separatey,true);*/
-		
-		//First, get the two object deltas
-		float overlap = 0;
-		float act1delta = actor1.location.y - actor1.last.y;
-		float act2delta = actor2.location.y - actor2.last.y;
-		if(act1delta != act2delta)
-		{
-			//Check if the y hulls actually overlap
-			float act1deltaAbs = (act1delta > 0)?act1delta:-act1delta;
-			float act2deltaAbs = (act2delta > 0)?act2delta:-act2delta;
-			Rectangle act1rect = actor1.zoneBox;//new Rectangle(actor1.location.y-((act1delta > 0)?act1delta:0),actor1.last.y,actor1.rect.Height+((act1delta > 0)?act1delta:-act1delta),actor1.height);
-			Rectangle act2rect = actor2.zoneBox;//new Rectangle(actor2.location.y-((act2delta > 0)?act2delta:0),actor2.last.y,actor2.rect.getHeight()+((act2delta > 0)?act2delta:-act2delta),actor2.height);
-			//if((act1rect.gety() + act1rect.getHeight() > act2rect.gety()) && (act1rect.gety() < act2rect.gety() + act2rect.Height) && (act1rect.y + act1rect.height > act2rect.y) && (act1rect.y < act2rect.y + act2rect.height))
-			{
-				float mayOverlap = act1deltaAbs + act2deltaAbs + 1;
-				//int mayOverlap = 1;
-				
-				//If they did overlap (and can), figure out by how much and flip the corresponding flags
-				if(act1delta > act2delta)
-				{
-					overlap = actor1.location.y + act1rect.getHeight() - actor2.location.y;
-					if((overlap > mayOverlap))// || !(actor1.allowCollisions & RIGHT) || !(actor2.allowCollisions & LEFT))
-						overlap = 0;
-					/*else
-					{
-						actor1.touching |= RIGHT;
-						actor2.touching |= LEFT;
-					}*/
-				}
-				else if(act1delta < act2delta)
-				{
-					overlap = actor1.location.y - act2rect.getHeight() - actor2.location.y;
-					if((-overlap > mayOverlap))// || !(actor1.allowCollisions & LEFT) || !(actor2.allowCollisions & RIGHT))
-						overlap = 0;
-					/*else
-					{
-						actor1.touching |= LEFT;
-						actor2.touching |= RIGHT;
-					}*/
-				}
-			}
-		}
-		
-		//Then adjust their positions and velocities accordingly (if there was any overlap)
-		if(overlap != 0)
-		{
-			float act1v = actor1.velocity.y;
-			float act2v = actor2.velocity.y;
-			
-			if(!act1immovable && !act2immovable)
-			{
-				overlap *= 0.5;
-				actor1.location.y = actor1.location.y - overlap;
-				actor2.location.y += overlap;
-
-				float act1velocity = (float) (Math.sqrt((act2v * act2v * actor2.mass)/actor1.mass) * ((act2v > 0)?1:-1));
-				float act2velocity = (float) (Math.sqrt((act1v * act1v * actor1.mass)/actor2.mass) * ((act1v > 0)?1:-1));
-				float average = (act1velocity + act2velocity)*0.5f;
-				act1velocity -= average;
-				act2velocity -= average;
-				actor1.velocity.y = average + act1velocity * actor1.elasticity;
-				actor2.velocity.y = average + act2velocity * actor2.elasticity;
-			}
-			else if(!act1immovable)
-			{
-				actor1.location.y = actor1.location.y - overlap;
-				actor1.velocity.y = act2v - act1v*actor1.elasticity;
-			}
-			else if(!act2immovable)
-			{
-				actor2.location.y += overlap;
-				actor2.velocity.y = act1v - act2v*actor2.elasticity;
 			}
 			return true;
 		}
