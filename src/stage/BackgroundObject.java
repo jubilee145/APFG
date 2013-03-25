e43de21bab2a6f7fc397bc5b8a94080d9e612f3e
Jubilee <jubileech@gmail.com>
2013-03-25 21:03:37 +1100

    Test Stage.
    Added background objects.
    Also replaced the movelist with one that will actually work. :/

---
+++ src/stage/BackgroundObject.java
@@ -0,0 +1,45 @@
+package stage;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+
+/**
+ * Essentially an image/animation with parallax. No need for collisions
+ * or any real logic. Maybe give them a means to follow paths or something
+ * later on.
+ * 
+ * @author Jubilee
+ */
+public class BackgroundObject {
+
+	public float distance;
+	float parallax;
+	public Vector2f location;
+	public Vector2f baseLocation;
+	public Color color;
+	Image image;
+	
+	public BackgroundObject(Image image, float parallax, Vector2f location, float distance)
+	{
+		this.distance = distance;
+		this.parallax = parallax;
+		this.baseLocation = location;
+		this.location = new Vector2f(0,0);
+		this.image = image;
+	}
+	
+	public void render(GameContainer container, Graphics g, float offsetX, float offsetY)
+			throws SlickException {
+		g.drawImage(image, location.x, location.y, color);
+	}
+	
+	public void setLocation(Vector2f location)
+	{
+		this.location = location.scale(parallax);
+		this.location.add(baseLocation);
+	}
+}
--- src/stage/Stage.java
+++ src/stage/Stage.java
@@ -1,5 +1,14 @@
 package stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+
 /**
  * The environment, background objects, floor, music, worldbounds.
  * Associated with a particular character, or just random?
@@ -7,6 +16,89 @@ package stage;
  * 
  * @author Jubilee
  */
+
+
 public class Stage {
 
+	private Music music;
+	public List<BackgroundObject> background;
+	
+	public Stage() throws SlickException
+	{
+		//Test background objects
+		background = new ArrayList<BackgroundObject>();
+		Image test = new Image("assets/stage/images/bgTestObject.png");
+		Image i;
+		Vector2f location;
+		float parallax;
+		BackgroundObject b;
+		{
+			i = test.copy();
+			location = new Vector2f(700,0);
+			parallax = 0.1f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(50,50,50);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(800,0);
+			parallax = 0.1f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(50,50,50);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(900,0);
+			parallax = 0.1f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(50,50,50);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(280,0);
+			parallax = 0.25f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(150,150,150);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(380,0);
+			parallax = 0.25f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(150,150,150);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(140,0);
+			parallax = 0.5f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(250,250,250);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(240,0);
+			parallax = 0.5f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(250,250,250);
+			background.add(b);
+			
+			i = test.copy();
+			location = new Vector2f(340,0);
+			parallax = 0.5f;
+			b = new BackgroundObject(i,parallax, location, 1);
+			b.color = new Color(250,250,250);
+			background.add(b);
+		}
+		//Set up music.
+		/*try {
+			music = new Music("assets/sounds/FF7BossBattlePiano_byTannerHelland.ogg");
+			music.loop();
+			music.play();
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Failure in Stage constructor: " + e.getMessage());
+			System.out.println(System.getProperty("user.dir"));
+			e.printStackTrace();
+		}*/
+	}
 }
