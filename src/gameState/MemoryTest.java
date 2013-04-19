fecaba7ef55d8b2de45403b666342ca194774487
jubilee145 <jubileech@gmail.com>
2013-04-19 20:05:23 +1000

    Update MemoryTest.java
    This might actually work now. :3
    Initial tests have this capping image-related memory use to about 500mb.

--- src/gameState/MemoryTest.java
+++ src/gameState/MemoryTest.java
@@ -2,6 +2,8 @@ package gameState;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +24,7 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 import org.newdawn.slick.util.ResourceLoader;
+import org.newdawn.slick.util.ResourceLocation;
 
 import entities.Fighter;
 
@@ -38,36 +41,38 @@ public class MemoryTest extends BasicGameState{
 	
 	List<SpriteSheet> sheetList;
 	Texture displayTexture, tempTexture;
-	Image displayImage;
+	Texture displayTexture2, tempTexture2;
+	Image displayImage, displayImage2;
 	File imageFolder;
 	File[] fileNames;
 	int fileCounter = 0;
+	int fileCounter2 = 1;
 
-	boolean firstTest, secondTest, thirdTest;
-	
-	long totalMem;
-	long maxMem;
-	long freeMem;
-	double megs;
+	boolean testActive;
 	
 	@Override
 	public void enter(GameContainer container, StateBasedGame game)
 	{
-		firstTest = secondTest = thirdTest = false;
+		testActive = false;
 	}
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 	        throws SlickException {
 	    this.game = game;
-	    imageFolder = new File("assets/memorytest");
+	    imageFolder = new File("assets/memoryTest");
 	    displayImage = new Image(20,20);
+	    displayImage2 = new Image(20,20);
 	    fileNames = imageFolder.listFiles();
 	    
 	    try {
 			tempTexture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileNames[fileCounter].getPath()));
 			displayTexture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileNames[fileCounter].getPath()));
 			displayImage.setTexture(displayTexture);
+			
+			tempTexture2 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileNames[fileCounter2].getPath()));
+			displayTexture2 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileNames[fileCounter2].getPath()));
+			displayImage2.setTexture(displayTexture2);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -77,30 +82,27 @@ public class MemoryTest extends BasicGameState{
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-        Runtime rt = Runtime.getRuntime();
-        totalMem = rt.totalMemory();
-        maxMem = rt.maxMemory();
-        freeMem = rt.freeMemory();
-        megs = 1048576.0;
-		
-        g.drawString ("Total Memory: " + totalMem + " (" + (totalMem/megs) + " MiB)", 20, 40);
-        g.drawString ("Max Memory:   " + maxMem + " (" + (maxMem/megs) + " 20)", 20, 60);
-        g.drawString ("Free Memory:  " + freeMem + " (" + (freeMem/megs) + " 20)", 20, 80);
-        
 		g.drawImage(displayImage, 200, 200);
+		g.drawImage(displayImage2, 400, 200);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
 		//System.gc();
-		if(firstTest)
+
+		if(testActive)
 		{
 			displayTexture = tempTexture;
 			displayImage.destroy();
 			displayImage.setTexture(displayTexture);
+			
+			displayTexture2 = tempTexture2;
+			displayImage2.destroy();
+			displayImage2.setTexture(displayTexture2);
 			try {
 				tempTexture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileNames[fileCounter + 1].getPath()));
+				tempTexture2 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(fileNames[fileCounter2].getPath()));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -110,6 +112,12 @@ public class MemoryTest extends BasicGameState{
 			{
 				//firstTest = false;
 				fileCounter = 0;
+			}
+			fileCounter2 --;
+			if(fileCounter2 <= 1)
+			{
+				//firstTest = false;
+				fileCounter2 = 500;
 			}
 		}
 		
@@ -126,7 +134,7 @@ public class MemoryTest extends BasicGameState{
         int abortKey4 = Manager.player2.inputHandler.keysIndex[7];
 
         if(key == acceptKey1||key == acceptKey2||key == acceptKey3||key == acceptKey4)
-        	firstTest = true;
+        	testActive = true;
         if(key == abortKey1||key == abortKey2||key == abortKey3||key == abortKey4)
         	game.enterState(Manager.StateIndex.MAIN_MENU.ordinal(), new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
     }
@@ -137,3 +145,4 @@ public class MemoryTest extends BasicGameState{
 	}
 
 }
+
